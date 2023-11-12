77c015631f49da9290fac2a938073fac322fb9de
unknown <ahmetsdogan16@gmail.com>
2023-11-12 11:40:55 +0300

    javascript üzerinden etiket oluşturma

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ input.setAttribute('placeholder', 'Değer')
 // console.log(input.getAttribute('placeholder'))
 //? Attribute silme
 // input.removeAttribute('placeholder')
+
+//! Javascriptten etiket oluşturma
+const div = document.createElement('div')
+div.style.width ="300px"
+div.style.height ="300px"
+div.style.border ="1px solid black"
+
+
+//* oluşturulan etiketi body'e gönderme
+document.body.appendChild(div)
+//* oluşturulan etiketi htmlden kaldırma
+document.body.removeChild(div)
+//! Javascriptten etiket oluşturma
