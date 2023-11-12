cd87aafeb90bc7272bc8adaabc4bc7efb86c3928
unknown <ahmetsdogan16@gmail.com>
2023-11-12 11:02:28 +0300

    html sayfama input ekledim js ile placeholderi getirdim

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log(countries)
+// console.log(countries)
+
+
+
+const input = document.getElementById('input')
+//? Attribute Ekleme 
+input.setAttribute('placeholder', 'Değer')
+//? Atribute içindeki bilgiyi getirme
+// console.log(input.getAttribute('placeholder'))
+//? Attribute silme
+// input.removeAttribute('placeholder')
